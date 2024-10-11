e212c43c22d46a172d014efdf6d7bf4f84215c11
qwerty <93528492+CodeAkyra@users.noreply.github.com>
2024-10-11 21:13:20 +0800

    Major Changes

--- src/main/includes/footer.php
+++ src/main/includes/footer.php
@@ -1,16 +1,6 @@
 </main>
 </div>
 
-
-
-<?php
-include('includes/modals/inventory-deliveries-modals.php');
-include('includes/modals/inventory-master-data-modals.php');
-include('includes/modals/inventory-release-materials-modals.php');
-include('includes/modals/inventory-view-batch-modals.php');
-include('includes/modals/production-release-material-modals.php');
-?>
-
 <?php
 include('includes/modal.php');
 include('includes/scripts.php');
